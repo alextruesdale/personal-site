70026fb8c506fe1b99f232586ec1d6fe815633f6
alextruesdale <alex.truesdale@colorado.edu>
2017-09-03 16:55:32 -0600

    fullscreen req.

--- _assets/js/nodes/image_preload.js
+++ _assets/js/nodes/image_preload.js
@@ -7,6 +7,18 @@ var imageFiles = [
 	"/images/404small.png",
 	"/images/404.png",
 	"/images/about_images/about_bkgd.png",
+	"/images/resume_images/Résumé.pdf",
+	"/images/gallery_images/photo_bkgd.png",
+	"/images/photography/teaser/Belgium.png",
+	"/images/photography/teaser/Berlin.png",
+	"/images/photography/teaser/Copenhagen.png",
+	"/images/photography/teaser/Dresden.png",
+	"/images/photography/teaser/Hamburg.png",
+	"/images/photography/teaser/Leipzig.png",
+	"/images/photography/teaser/Norway.png",
+	"/images/photography/teaser/Paris.png",
+	"/images/photography/teaser/Prague.png",
+	"/images/photography/teaser/Stralsund.png"
 ];
 
 function preload(imageFiles) {
