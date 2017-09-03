3bd5f44e012d4d9c51547a8ba9938ce858eadfca
alextruesdale <alex.truesdale@colorado.edu>
2017-09-03 17:02:01 -0600

    lighten js

--- _assets/js/nodes/image_preload.js
+++ _assets/js/nodes/image_preload.js
@@ -9,16 +9,6 @@ var imageFiles = [
 	"/images/about_images/about_bkgd.png",
 	"/images/resume_images/Résumé.pdf",
 	"/images/gallery_images/photo_bkgd.png",
-	"/images/photography/teaser/Belgium.png",
-	"/images/photography/teaser/Berlin.png",
-	"/images/photography/teaser/Copenhagen.png",
-	"/images/photography/teaser/Dresden.png",
-	"/images/photography/teaser/Hamburg.png",
-	"/images/photography/teaser/Leipzig.png",
-	"/images/photography/teaser/Norway.png",
-	"/images/photography/teaser/Paris.png",
-	"/images/photography/teaser/Prague.png",
-	"/images/photography/teaser/Stralsund.png"
 ];
 
 function preload(imageFiles) {
